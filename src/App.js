f565afad82434788b608db0a40c94d7570a118d6
josiahprathaban <josiah.prathaban@gmail.com>
2023-10-20 10:29:14 +0530

    basic layout upadated

--- src/App.js
+++ src/App.js
@@ -165,22 +165,22 @@ function App() {
           </span>
           <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
             <li>
-              <a href="#" class="mr-4 hover:underline md:mr-6 ">
+              <a href="https://keture.com/" class="mr-4 hover:underline md:mr-6 ">
                 About
               </a>
             </li>
             <li>
-              <a href="#" class="mr-4 hover:underline md:mr-6">
+              <a href="https://keture.com/" class="mr-4 hover:underline md:mr-6">
                 Privacy Policy
               </a>
             </li>
             <li>
-              <a href="#" class="mr-4 hover:underline md:mr-6">
+              <a href="https://keture.com/" class="mr-4 hover:underline md:mr-6">
                 Licensing
               </a>
             </li>
             <li>
-              <a href="#" class="hover:underline">
+              <a href="https://keture.com/" class="hover:underline">
                 Contact
               </a>
             </li>
