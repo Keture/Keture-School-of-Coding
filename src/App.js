a6aa215132dc5fff734bc029230c8585a98e8592
Ben <50701475+hanancs@users.noreply.github.com>
2023-10-20 15:29:42 +0530

    add home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 
-import React, { useState } from "react";
-import { Header, CourseDetails, Instructor, Syllabus, Footer, CourseIntro } from "./components";
+import React from "react";
+import Home from "./pages/Home";
 
 function App() {
-  const [open, setOpen] = useState(1);
-  const handleOpen = (value) => setOpen(open === value ? 0 : value);
 
   return (
     <>
-      <div className="container max-w-3xl mx-auto mt-16 text-slate-700">
-        <Header />
-        <CourseIntro />
-        <div className="md:grid grid-cols-3 mt-10 gap-4 md:px-4 lg:px-0">
-          <CourseDetails />
-          <Instructor />
-        </div>
-        <Syllabus open={open} handleOpen={handleOpen} />
-      </div>
-      <Footer />
+      <Home />
     </>
   );
 }
